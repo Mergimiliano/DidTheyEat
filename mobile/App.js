67aecc14dba99076fbd44d796f56cb6d202fdbab
Massimiliano Melillo <m.melillo148@gmail.com>
2024-11-21 12:30:02 +0100

    WIP:Routing almost working with a placeholder loading

--- mobile/App.js
+++ mobile/App.js
@@ -8,8 +8,8 @@ import GetStarted from './src/screens/GetStarted';
 import Register from './src/screens/Register';
 import Login from './src/screens/Login';
 import Profile from './src/screens/Profile';
-import { useNavigation } from '@react-navigation/native';
 import { refreshToken } from './src/services/tokenService';
+import { ActivityIndicator, View } from 'react-native';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -22,7 +22,7 @@ const AppTabs = () => (
 );
 
 export default function App() {
-  const navigation = useNavigation();
+  const [initialRoute, setInitialRoute] = useState(null);
 
   useEffect(() => {
     const checkAuthStatus = async () => {
@@ -31,31 +31,37 @@ export default function App() {
         if (tokens) {
           const refreshSuccess = await refreshToken(tokens.username);
           if (refreshSuccess) {
-            navigation.replace('AppTabs'); 
+            setInitialRoute('AppTabs');
           } else {
-            navigation.replace('Login');
+            setInitialRoute('Login');
           }
         } else {
-          navigation.replace('GetStarted')
+          setInitialRoute('GetStarted');
         }
       } catch (error) {
         console.error('Error checking auth status:', error);
-        navigation.replace('Login');
+        setInitialRoute('Login');
       }
     };
 
     checkAuthStatus();
-  }, [navigation]);
+  }, []);
 
-  // TO DO LOADING
+  if (!initialRoute) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="GetStarted" component={GetStarted} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="AppTabs" component={AppTabs} />
+      <Stack.Navigator initialRouteName={initialRoute}>
+        <Stack.Screen name="GetStarted" component={GetStarted} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="AppTabs" component={AppTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
