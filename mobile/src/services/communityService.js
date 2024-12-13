bb363bd974c8cd7fa86e68f25b851fba5671a29e
Massimiliano Melillo <m.melillo148@gmail.com>
2024-12-13 17:41:32 +0100

    WIP: Update and Delete Community

--- mobile/src/services/communityService.js
+++ mobile/src/services/communityService.js
@@ -1,13 +1,31 @@
 import axiosInstance from '../utils/axiosInstance';
 
+export const getCommunities = () => {
+  return axiosInstance.get('/communities/')
+    .then(response => response.data)
+    .catch(err => { throw err; });
+};
+
+export const getCommunity = (id) => {
+  return axiosInstance.get(`/communities/${id}`)
+    .then(response => response.data)
+    .catch(err => { throw err; });
+};
+
 export const createCommunity = (communityData) => {
   return axiosInstance.post('/communities/', communityData)
     .then(response => response.data)
     .catch(err => { throw err; });
 };
 
-export const getCommunities = () => {
-  return axiosInstance.get('/communities/')
+export const updateCommunity = (id, communityData) => {
+  return axiosInstance.patch(`/communities/${id}`, communityData)
+    .then(response => response.data)
+    .catch(err => { throw err; });
+};
+
+export const deleteCommunity = (id) => {
+  return axiosInstance.delete(`/communities/${id}`)
     .then(response => response.data)
     .catch(err => { throw err; });
 };
